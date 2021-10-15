b16e29d60fb64d579020d23f93e13128ceebc2e6
EfeDaniel <EfeEjemudaro@gmail.com>
2021-10-15 15:54:51 +0100

    Add fragment structures

---
+++ app/src/main/java/com/efedaniel/ulesson/ulessonapp/screens/live/LiveViewModel.kt
@@ -0,0 +1,7 @@
+package com.efedaniel.ulesson.ulessonapp.screens.live
+
+import androidx.lifecycle.ViewModel
+
+class LiveViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
---
+++ app/src/main/java/com/efedaniel/ulesson/ulessonapp/screens/me/MeViewModel.kt
@@ -0,0 +1,7 @@
+package com.efedaniel.ulesson.ulessonapp.screens.me
+
+import androidx.lifecycle.ViewModel
+
+class MeViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
