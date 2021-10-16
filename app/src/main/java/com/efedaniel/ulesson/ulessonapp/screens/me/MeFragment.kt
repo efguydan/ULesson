fd1970c17ee1e82c980bcf6422460cb3ee965059
EfeDaniel <EfeEjemudaro@gmail.com>
2021-10-16 08:29:11 +0100

    Add Me Fragment UI

--- app/src/main/java/com/efedaniel/ulesson/ulessonapp/screens/me/MeFragment.kt
+++ app/src/main/java/com/efedaniel/ulesson/ulessonapp/screens/me/MeFragment.kt
@@ -6,17 +6,34 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.lifecycle.ViewModelProviders
 import com.efedaniel.ulesson.R
+import com.efedaniel.ulesson.base.BaseFragment
+import com.efedaniel.ulesson.databinding.FragmentLiveBinding
+import com.efedaniel.ulesson.databinding.FragmentMeBinding
+import com.efedaniel.ulesson.ulessonapp.screens.live.LiveViewModel
+import javax.inject.Inject
 
-class MeFragment : Fragment() {
+class MeFragment : BaseFragment() {
 
-//    private lateinit var viewModel: MeViewModel
+    @Inject
+    lateinit var viewModelFactory: ViewModelProvider.Factory
+
+    private lateinit var viewModel: MeViewModel
+    private lateinit var binding: FragmentMeBinding
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        return inflater.inflate(R.layout.fragment_me, container, false)
+    ): View {
+        binding = FragmentMeBinding.inflate(layoutInflater)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        daggerAppComponent.inject(this)
+        viewModel = ViewModelProviders.of(this, viewModelFactory).get(MeViewModel::class.java)
     }
 
 }
